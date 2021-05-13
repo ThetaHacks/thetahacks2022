7b7b76d87c5287e246ac533b06f7e012b8b1567c
Will Lane <williamlane923@gmail.com>
2021-05-12 17:46:34 -0700

    refactor(App.svelte Header.svelte): moved header to seperate component file

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
+  import Header from './components/Header.svelte';
 </script>
 
 <main>
-  <h1 class="named"><span class="blue">Theta</span><span class="pink">Hacks</span></h1>
+  <Header />
 </main>
 
-<style lang="sass">
-  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap')
-
-  .named
-    font-family: 'Raleway', sans-serif
-    text-align: center
-
-    font-weight:  9000
-    font-size: 11em
-
-    .blue
-      color: #B8D3D1
-
-    .pink
-      color: #CE7B91
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap');
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="named"><span class="blue">Theta</span><span class="pink">Hacks</span></h1>
+</template>
+
+<style lang="sass">
+  .named
+    font-family: 'Raleway', sans-serif
+    text-align: center
+
+    font-weight: 9000
+    font-size: 11vh
+
+    .blue
+      color: #B8D3D1
+
+    .pink
+      color: #CE7B91
+</style>

fc8b847ebd6e95758c87f488864a6f22a46c9c75
Will Lane <williamlane923@gmail.com>
2021-05-12 18:08:09 -0700

    feature(header): added description and discord button

---
+++ src/components/About.svelte
@@ -0,0 +1,7 @@
+<template>
+
+</template>
+
+<style lang="sass">
+
+</style>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,18 +1,37 @@
+<script lang="ts">
+  import HeaderButtons from './HeaderButtons.svelte';
+</script>
+
 <template>
-  <h1 class="named"><span class="blue">Theta</span><span class="pink">Hacks</span></h1>
+  <div class="header-wrapper">
+    <h1 class="named"><span class="blue">Theta</span><span class="pink">Hacks</span></h1>
+    <h2 class="lose">
+      Lose yourself in creativity.
+    </h2>
+
+    <HeaderButtons discord_background="#B8D3D1" />
+  </div>
 </template>
 
 <style lang="sass">
-  .named
+  h1, h2, h3, h4, h5, p, span
     font-family: 'Raleway', sans-serif
+    color: white
+
+  .header-wrapper
     text-align: center
 
-    font-weight: 9000
-    font-size: 11vh
+    .named
+      font-weight: 9000
+      font-size: 11vh
 
-    .blue
-      color: #B8D3D1
+      .blue
+        color: #B8D3D1
 
-    .pink
-      color: #CE7B91
+      .pink
+        color: #CE7B91
+
+    .lose
+      margin-top: -5vh
+      font-size: 4vh
 </style>

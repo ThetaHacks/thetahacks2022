5c9b5cc4ffaa5c11b865c76790c5cd54b408762a
Kai Devrim <kkgit@devrim.tech>
2021-07-05 14:17:27 +0300

    Setup for ThetaHacks

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,13 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     project: resolve(__dirname, './tsconfig.json'),
     tsconfigRootDir: __dirname,
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module' // Allows for the use of imports
   },
 
   env: {
-    browser: true
+    browser: true,
+    es6: true
   },
 
   // Rules order is important, please avoid shuffling them
